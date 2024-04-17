101c4a12f3ce7a1651f4a11e54b32c1ae9777694
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-04-17 14:12:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import { HomePage } from './pages/HomePage/HomePage'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
